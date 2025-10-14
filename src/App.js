ac28ae6ffa07f09b78f4716270c3af46bd9f3061
Professor Dev <isma.salgeriev@bk.ru>
2025-10-14 14:49:55 +0300

    The new lesson about JSON

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from 'react';
-import { Header } from './components/Header';
-import { Main } from './components/Main';
-import { Footer } from './components/Footer';
+import React, { useEffect, useState } from 'react';
 
 
 
 export const App = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading ] = useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json);
+        setLoading(false);
+      }); 
+  }, [])
   return (
     <div className="App">
-      <Header/>
-      <Main/>
-      <Footer/>
+      <h1 style={{textAlign: 'center', color: '#090'}}>Counter App</h1>
+      <h4><a style={{color: 'lightgreen'}}>completed: true</a> <a style={{color: 'lightcoral'}}>completed: false</a></h4>
+      <ul>
+     {
+      loading ? <h2>Loading...</h2> : data.map((item) => {
+          return (
+            <div key={item.id}>
+              {item.completed ? <li id='trueLi'>{item.title}</li> : <li id='falseLi'>{item.title}</li>}
+            </div>
+          )
+        })
+      }
+      </ul>
     </div>
   )
 }
-
 
--- src/index.js
+++ src/index.js
@@ -2,37 +2,11 @@ import React, { act } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
 
-
-
-const reducer = (state = 0, action) => {
-
-    switch (action.type) {
-        case "plus":
-            return state + 1
-
-        case "minus":
-            if(state > 0) {
-                return state - 1
-            }
-        
-        case "reset":
-            return 0
-
-        default:
-            return state
-    }
-}
-
-const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
         <App />
-    </Provider>
 );
 
